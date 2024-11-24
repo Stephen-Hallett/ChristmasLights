3f861f0a659550b3a0af0ffa8a3a3638b4ee7a4a
Stephen-Hallett <stevohallett@gmail.com>
2024-11-24 18:49:03 +1300

    feat: Add error check to config file read

--- frontend/utils/utilities.py
+++ frontend/utils/utilities.py
@@ -5,5 +5,5 @@ import toml
 
 def load_streamlit_config(path: str = "./.streamlit/config.toml") -> dict:
     with Path(path).open() as f:
-        config = toml.load(f)
-    return config
+        return toml.load(f)
+    return FileExistsError(f'File not found at "{path}"')
