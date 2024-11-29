a92885cfc4c0cb85a616bef46789d8e8c673d156
Stephen-Hallett <stevohallett@gmail.com>
2024-11-29 21:26:08 +1300

    feat: Add active column and enforce unique index

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,11 @@
 CREATE TABLE Patterns (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50) NOT NULL UNIQUE,
-    pattern TEXT NOT NULL
+    pattern TEXT NOT NULL,
+    active INTEGER NOT NULL
 );
+
+CREATE UNIQUE INDEX one_active_pattern ON Patterns(active) WHERE active = 1;
 
 CREATE TABLE Effects (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,5 +15,5 @@ CREATE TABLE Effects (
     FOREIGN KEY (id) REFERENCES Patterns(id)
 );
 
-INSERT INTO Patterns (name, pattern) VALUES ('Candy Cane', '["#FFFFFF", "#ff2612"]');
+INSERT INTO Patterns (name, pattern, active) VALUES ('Candy Cane', '["#FFFFFF", "#ff2612"]', 1);
 INSERT INTO Effects (id, breathing, chasing, sparkle) VALUES (1, 0, 0.5, 1);
