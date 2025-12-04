4d5deb66b54b87d2129bb0f5885e5d181366a8e4
Stephen-Hallett <stevohallett@gmail.com>
2025-12-04 21:46:30 +1300

    fix: Cast env variable to int

--- client/ChristmasClient.py
+++ client/ChristmasClient.py
@@ -1,14 +1,13 @@
 import asyncio
 import math
-import time
 import os
-
-from utilities import getAlpha, getPattern
+import time
 
 from ChristmasLights import ChristmasLights
+from utilities import getAlpha, getPattern
 
 # LED strip configuration:
-LED_COUNT = os.environ["N_LEDS"]  # Number of LED pixels.
+LED_COUNT = int(os.environ["N_LEDS"])  # Number of LED pixels.
 LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
